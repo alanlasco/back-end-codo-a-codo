61d955c635f56ec2264ab721b3341fb2c9e98c88
DixyStardust <dixystardust@gmail.com>
2024-06-23 20:25:55 -0300

    libros.controller y libros.router

--- controllers/libros.controller.js
+++ controllers/libros.controller.js
@@ -1,1 +1,11 @@
 console.log("controlador de libros");
+
+const db = require("../db/db");
+const fs = require("fs");
+const path = require("path");
+module.exports = {
+  index,
+  show,
+  store,
+  destroy,
+};
--- routes/libros.router.js
+++ routes/libros.router.js
@@ -1,1 +1,17 @@
 console.log("rutas de libros");
+
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/upload");
+
+const controller = require("../controllers/libros.controller");
+
+// Prefijo: /libros
+
+router.get("/", controller.index);
+router.get("/:id", controller.show);
+router.post("/", upload.single("imagen"), controller.store);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.destroy);
+
+module.exports = router;
