5f59ec53a8b2abef6367d8ec902c64c7c0d559b7
Sbravito <santiagobravo22@hotmail.com>
2024-06-23 21:54:11 -0300

    controller and router

---
+++ routes/autores.router.js
@@ -0,0 +1,16 @@
+console.log("rutas de autores");
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/upload");
+
+const controller = require("../controllers/autores.controller");
+
+// Prefijo: /autores
+
+router.get("/", controller.index);
+router.get("/:id", controller.show);
+router.post("/", upload.single("imagen"), controller.store);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.destroy);
+
+module.exports = router;
