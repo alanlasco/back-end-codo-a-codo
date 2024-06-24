b4de487cc6ca79dbfd617d0397e02db377210ee8
Sbravito <santiagobravo22@hotmail.com>
2024-06-23 22:47:02 -0300

    acomodando

--- controllers/autores.controller.js
+++ controllers/autores.controller.js
@@ -2,46 +2,43 @@ console.log("controlador de autores");
 const db = require("../db/db");
 const fs = require("fs");
 const path = require("path");
-
+// comentario para acomodar
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "public/uploads");
-    },
-    filename: (req, file, cb) => {
-      // console.log(file);
-      cb(null, Date.now() + path.extname(file.originalname));
-    },
-  });
-  
-  const upload = multer({
-    storage,
-    fileFilter: (req, file, cb) => {
-      // console.log(file);
-      const fileTypes = /jpg|jpeg|png/;
-  
-      const mimetype = fileTypes.test(file.mimetype);
-  
-      const extname = fileTypes.test(
-        path.extname(file.originalname).toLowerCase()
-      );
-  
-      if (mimetype && extname) {
-        return cb(null, true);
-      }
-  
-      cb("Tipo de archivo no soportado");
-    },
-    limits: { fileSize: 1024 * 1024 * 1 }, // 1 Mb
-  });
-  
-  const controller = require("../controllers/productos.controller");
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    // console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    // console.log(file);
+    const fileTypes = /jpg|jpeg|png/;
 
+    const mimetype = fileTypes.test(file.mimetype);
+
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    cb("Tipo de archivo no soportado");
+  },
+  limits: { fileSize: 1024 * 1024 * 1 }, // 1 Mb
+});
+
+const controller = require("../controllers/productos.controller");
 
 module.exports = {
   index,
   show,
   store,
   destroy,
-  
 };
