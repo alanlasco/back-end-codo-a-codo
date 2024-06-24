f4da188a3655a7014e029a6366598412d7498fb0
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-06-23 22:12:01 -0300

    agregar metodo index y show

--- controllers/juegos.controller.js
+++ controllers/juegos.controller.js
@@ -2,6 +2,35 @@ const db = require("../db/db");
 const fs = require("fs");
 const path = require("path");
 
+const index = (req, res) => {
+  const sql =
+    "SELECT j.id_juegos, j.nombre_juego, j.imagen_juego, j.url_juego, j.plataformas_id, p.plataforma FROM juegos AS j INNER JOIN plataformas AS p ON j.plataformas_id = p.id_plataformas";
+  db.query(sql, (error, rows) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+    res.json(rows);
+  });
+};
+const show = (req, res) => {
+  const { id } = req.params;
+
+  const sql =
+    "SELECT j.id_juegos, j.nombre_juego, j.imagen_juego, url_juego, j.plataformas_id, p.plataforma FROM juegos AS j INNER JOIN plataformas AS p ON j.plataformas_id = p.id_plataformas WHERE j.id_juegos = ?";
+  db.query(sql, [id], (error, rows) => {
+    // console.log(rows);
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    if (rows.length == 0) {
+      return res.status(404).send({ error: "No existe el juego" });
+    }
+
+    res.json(rows[0]);
+  });
+};
+
 const destroy = (req, res) => {
   const { id } = req.params;
   //selecciono el registro con id que tiene la imagen a borrar
@@ -29,7 +58,7 @@ const destroy = (req, res) => {
     }
 
     if (result.affectedRows == 0) {
-      return res.status(404).send({ error: "No existe el producto" });
+      return res.status(404).send({ error: "No existe el juego" });
     }
 
     res.json({ mensaje: "Juego eliminado" });
