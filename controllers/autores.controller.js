1e7f1632502ae359736d04ab20796586c65eb563
Sbravito <santiagobravo22@hotmail.com>
2024-06-23 23:14:56 -0300

    index and destroy

--- controllers/autores.controller.js
+++ controllers/autores.controller.js
@@ -2,39 +2,111 @@ console.log("controlador de autores");
 const db = require("../db/db");
 const fs = require("fs");
 const path = require("path");
-// comentario para acomodar
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    // console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
 
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    // console.log(file);
-    const fileTypes = /jpg|jpeg|png/;
+const index = (req, res) => {
+  const sql = "SELECT * FROM autores";
+  db.query(sql, (error, rows) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+    res.json(rows);
+  });
+};
 
-    const mimetype = fileTypes.test(file.mimetype);
+// const show = (req, res) => {
+//   const { id } = req.params;
 
-    const extname = fileTypes.test(
-      path.extname(file.originalname).toLowerCase()
-    );
+//   const sql = "SELECT * FROM autores WHERE id = ?";
+//   db.query(sql, [id], (error, rows) => {
+//     // console.log(rows);
+//     if (error) {
+//       return res.status(500).json({ error: "Intente mas tarde" });
+//     }
 
-    if (mimetype && extname) {
-      return cb(null, true);
+//     if (rows.length == 0) {
+//       return res.status(404).send({ error: "No existe el producto" });
+//     }
+
+//     res.json(rows[0]);
+//   });
+// };
+
+// const store = (req, res) => {
+//   console.log(req.file);
+
+//   let imageName = "";
+
+//   if (req.file) {
+//     imageName = req.file.filename;
+//   }
+//   const { nombre, precio, stock } = req.body;
+
+//   const sql =
+//     "INSERT INTO productos (nombre, stock, precio, imagen) VALUES (?, ?, ?, ?)";
+//   db.query(sql, [nombre, stock, precio, imageName], (error, result) => {
+//     // console.log(result);
+//     if (error) {
+//       // console.log(error)
+//       return res.status(500).json({ error: "Intente mas tarde" });
+//     }
+
+//     const producto = { ...req.body, id: result.insertId };
+
+//     res.status(201).json(producto);
+//   });
+// };
+
+// const update = (req, res) => {
+//   const { id } = req.params;
+//   const { nombre, precio, stock } = req.body;
+
+//   const sql =
+//     "UPDATE productos SET nombre = ?, precio = ?, stock = ? WHERE id = ?";
+//   db.query(sql, [nombre, precio, stock, id], (error, result) => {
+//     console.log(result);
+//     if (error) {
+//       return res.status(500).json({ error: "Intente mas tarde" });
+//     }
+
+//     if (result.affectedRows == 0) {
+//       return res.status(404).send({ error: "No existe el producto" });
+//     }
+
+//     const producto = { ...req.body, ...req.params };
+
+//     res.json(producto);
+//   });
+// };
+
+const destroy = (req, res) => {
+  const { id } = req.params;
+
+  let sql = "SELECT * FROM autores WHERE id_autores = ?";
+  db.query(sql, [id], (error, rows) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
     }
 
-    cb("Tipo de archivo no soportado");
-  },
-  limits: { fileSize: 1024 * 1024 * 1 }, // 1 Mb
-});
+    if (rows.length == 0) {
+      return res.status(404).send({ error: "No existe el autor" });
+    }
 
-const controller = require("../controllers/productos.controller");
+    fs.unlinkSync(path.resolve(__dirname, "../public/uploads", rows[0].imagen));
+  });
+
+  sql = "DELETE FROM autores WHERE id_autores = ?";
+  db.query(sql, [id], (error, result) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    if (result.affectedRows == 0) {
+      return res.status(404).send({ error: "No existe el autor" });
+    }
+
+    res.json({ mensaje: "Autor eliminado" });
+  });
+};
 
 module.exports = {
   index,
