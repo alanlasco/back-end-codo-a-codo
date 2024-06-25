a73c34fc89bbb9bcdac824f8b733c29c9453ec60
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-06-25 11:57:16 -0300

    fixes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use("/juegos", require("./routes/juegos.router"));
 
+//te agregue la ruta de autores
+app.use("/autores", require("./routes/autores.router"));
+
 const PORT = 3306;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
