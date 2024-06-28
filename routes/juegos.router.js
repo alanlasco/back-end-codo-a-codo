dde90261596b802c0c166e9cf166c78caae461ca
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-06-28 18:46:57 -0300

    fixes

--- routes/juegos.router.js
+++ routes/juegos.router.js
@@ -1,19 +1,16 @@
-
 const express = require("express");
 const router = express.Router();
 
 const upload = require("../middleware/upload");
 
-
 const controller = require("../controllers/juegos.controller");
-// Prefijo: /juegos
 
+// Prefijo: /juegos
 
 router.get("/", controller.index);
 router.get("/:id", controller.show);
 router.put("/:id", upload.single("imagen"), controller.update);
 router.post("/", upload.single("imagen"), controller.store);
 router.delete("/:id", controller.destroy);
-
 
 module.exports = router;
